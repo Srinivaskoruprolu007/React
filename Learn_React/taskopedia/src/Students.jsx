ec618b2c540c90d0c8b968eacdf1d4e15a3a5a0d
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2023-09-18 14:20:34 +0530

    student added

---
+++ Learn_React/taskopedia/src/Students.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/alt-text */
+import {faker} from "@faker-js/faker";
+function Student(props) {
+  return (
+    <div className="col-4 p-1">
+      <div className="row boarder">
+        <div className="col-2">
+          <img src={faker.image.avatar()} className="w-100"></img>
+        </div>
+        <div className="col-8">
+          {props.fullname} <br />
+          Coding Experience {props.ProgrammingExp} years
+        </div>
+        <div className="col-2">
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Student;
