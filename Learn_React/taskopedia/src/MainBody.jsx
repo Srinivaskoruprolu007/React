3cf563b075412ca6067d56ad54cd3ca50583c704
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2023-09-18 14:19:40 +0530

    MainBody added

--- Learn_React/taskopedia/src/MainBody.jsx
+++ Learn_React/taskopedia/src/MainBody.jsx
@@ -1,4 +1,5 @@
 import Student from "./Students";
+import Studentreview from "./StudentReview";
 export default function MainBody() {
   const course = "React JS";
   const totalLectures = 3;
@@ -12,9 +13,13 @@ export default function MainBody() {
       </ul>
       <div className="px-4">Students enrolled</div>
       <div className="container px-4  ">
-        <Student fullname="srinivas" ProgrammingExp={2} />
-        <Student fullname="Manoj" ProgrammingExp={5} />
-        <Student fullname="Siva" ProgrammingExp={4} />
+        <Student fullname="srinivas" ProgrammingExp={2} >
+            <Studentreview/>
+        </Student>
+        <Student fullname="Manoj" ProgrammingExp={5} >
+            <Studentreview/>
+        </Student>
+        <Student fullname="Siva" ProgrammingExp={4} ></Student>
       </div>
     </div>
   );
