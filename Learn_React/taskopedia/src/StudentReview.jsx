85704d86f2bd427fb9e12aa08897c59a36c3cc8d
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2023-09-18 14:19:53 +0530

    Student review added

---
+++ Learn_React/taskopedia/src/StudentReview.jsx
@@ -0,0 +1,8 @@
+export default function Studentreview(){
+    return(
+        <div className="p-2">
+            <i className="bi bi-hand-thumbs-up-fill text-success p-1"></i> 
+            <i className="bi bi-hand-thumbs-up-fill text-danger p-1"></i>
+        </div>
+    )
+}
